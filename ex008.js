5558c981fc209b46ec0a50f3cde26a6d41b78a60
lucas mendes <lucasmndes.dev@gmail.com>
2024-10-08 15:14:29 -0300

    exercicios de if e else

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`a velocidade do seu carro é ${vel} km por hora`)
+if (vel > 60) {
+    console.log('voce ultrapassou a velocidade permitida. multado!')
+}
+console.log('diriga sempre com cinto de segurança')

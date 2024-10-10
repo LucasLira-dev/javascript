50fba529ea7f814847ffba38d8578774a9787fcc
lucas mendes <lucasmndes.dev@gmail.com>
2024-10-10 14:12:33 -0300

    exercicios

---
+++ ex011.js
@@ -0,0 +1,14 @@
+var idade = 67
+console.log(`voce tem ${idade} anos`)
+if (idade < 16) {
+    console.log('não vota')
+} else {
+    if (idade >= 16 && idade < 18 || idade > 65
+    ) {
+        console.log ('voto opcional')
+    } else {
+        console.log('voto obrigatorio')
+    }
+    
+
+    }
---
+++ treinamento/exercicio.js
@@ -0,0 +1,12 @@
+
+var nome = 'lucas'
+
+switch(nome) {
+    case 'lucas':
+        console.log('lindo')
+        break
+
+    default:
+        console.log('feio')
+        break
+}

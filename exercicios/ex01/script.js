0341b81da9713cf8d3093f5e83abdfbc855a9b18
lucas mendes <lucasmndes.dev@gmail.com>
2024-10-14 15:22:40 -0300

    fim do primeiro exercicio

--- exercicios/ex01/script.js
+++ exercicios/ex01/script.js
@@ -2,15 +2,17 @@ function carregar() {
 var hora = document.getElementById('msg')
 var img = document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 8
-msg.innerHTML = `agora são ${hora} horas`
+var hora = data.getHours()
+var minuto = data.getMinutes()
+msg.innerHTML = `agora são ${hora} horas e ${minuto} minutos`
 if (hora >= 0 && hora <= 12) {
    img.src = '../aula12/ex/modelo/manha.png.jpg'
-   foto.style.background = '#41371A'
+   document.body.style.background = '#41371A'
 } else if (hora >= 12 && hora <= 18) {
     img.src = '../aula12/ex/modelo/tarde.png.jpg'
+    document.body.style.background = '#472B1D'
 } else {
     img.src = '../aula12/ex/modelo/noite.png.jpg'
+    document.body.style.background = '#183442'
 }
  }

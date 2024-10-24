b3d48fe0ece2789b771910eae36e66f52ab34d02
lucas mendes <lucasmndes.dev@gmail.com>
2024-10-24 15:12:30 -0300

    exercicio

--- exercicios/aula14ex/script.js
+++ exercicios/aula14ex/script.js
@@ -2,17 +2,25 @@ function contagem() {
     var tn1 = document.getElementById('xinicio')
     var tn2 = document.getElementById('xfim')
     var tn3 = document.getElementById('xpasso')
-    var inicio = Number(tn1.value)
-    var fim = Number(tn2.value)
-    var passo = Number(tn3.value)
+    
     var res = document.getElementById('resultado')
-    if (inicio == 0 || fim == 0 || passo == 0) {
+    if (tn1.value.length == 0 || tn2.value.length == 0 || tn3.value.length == 0) {
         window.alert('erro, será atribuido o número 1')
         res.innerHTML = 'impossivel contar'
     } else {
+        var inicio = Number(tn1.value)
+        var fim = Number(tn2.value)
+        var passo = Number(tn3.value)
+        res.innerHTML= 'contando'
 
-    for(var inicio; inicio <= fim; inicio += passo) {
-        res.innerHTML += `${inicio}`
+        if(inicio < fim) {
+            for(var c = inicio; c <= fim; c += passo) {
+             res.innerHTML += `${c} \u{1FAE0}	`
     }
-}
-}
+    } else {
+    for(var c = inicio; c >= fim; c -= passo) {
+        res.innerHTML += `${c} \u{1FAE0}`
+    }
+    }
+    }
+    }

61c255515a2aaf6e12dd08d86534783e6812436a
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-09 13:32:15 -0300

    switch case

---
+++ treinamento/CFBcursos/switchcase.js
@@ -0,0 +1,41 @@
+let nome= "lucas"
+
+console.log('teste de beleza')
+
+
+switch(nome){
+    case "adao":{
+        console.log('adão é o mais lindo do grupo')
+        break
+    }
+
+    case "daniel":{
+        console.log('daniel é o mais lindo do grupo')
+        break
+    }
+
+    case "lucas":{
+        console.log('lucas é o mais lindo do grupo')
+        break
+    }
+
+    case "otavio1": {
+        console.log('otavio1 é o mais lindo do grupo')
+        break
+    }
+
+    case "otavio2":{
+        console.log('otavio2 é o mais lindo do grupo')
+        break
+    }
+
+    case "pedro":{
+        console.log('pedro é o mais lindo do grupo')
+        break
+    }
+
+}
+    
+
+
+

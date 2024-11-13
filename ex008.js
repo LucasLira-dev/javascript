9727a7186bf2e7963168a21b521110c506f9ec3a
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-13 15:39:56 -0300

    treino

--- ex008.js
+++ ex008.js
@@ -1,6 +1,9 @@
-var vel = 30
+var vel = 80
+var nome = "joao"
 console.log(`a velocidade do seu carro é ${vel} km por hora`)
-if (vel > 60) {
-    console.log('voce ultrapassou a velocidade permitida. multado!')
+if (vel > 60 && nome=="lucas") {
+    console.log(`olá ${nome} voce ultrapassou a velocidade permitida. multado!`)
+} else{
+    console.log(`olá ${nome} voce não ultrapassou a velocidade permitida. Boa!`)
 }
 console.log('diriga sempre com cinto de segurança')

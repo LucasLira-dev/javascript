a5445cbfa6d09be770502bf9dc6bbddc2824835a
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-15 13:17:54 -0300

    metodo map

---
+++ exercicios/CFBcursos/map.js
@@ -0,0 +1,17 @@
+// const cursos = ['HTML', 'CSS', 'JavaScript']
+
+// cursos.map((el, i)=>{
+//     console.log(`curso ${el} na posição ${i}`)
+// })
+
+
+// let el = document.getElementsByTagName('div')
+    
+// })
+
+const converterNum = (e)=> parseInt(e)
+const dobrar = (e)=> e*2
+
+let numero= ['1', '2', '3'].map(dobrar)
+
+console.log(numero)

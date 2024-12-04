a93f0aa7360bc54bc7018fd35983f9c37f8a44d6
lucas mendes <lucasmndes.dev@gmail.com>
2024-12-04 15:27:45 -0300

    objetos com function

--- exercicios/CFBcursos/objetos2.js
+++ exercicios/CFBcursos/objetos2.js
@@ -1,8 +1,9 @@
 
 
 
+// modelo com CLASS//
 
-class pessoa {
+/*class pessoa {
     constructor(pnome, pidade){
         this.nome= pnome
         this.idade= pidade
@@ -23,7 +24,33 @@ class pessoa {
     setIdade(idade){
         this.nome= idade
     }
-}
+} */
+
+
+    //modelo com FUNÇAO//
+
+    function Pessoa(pnome,pidade) {
+            this.nome= pnome,
+            this.idade= pidade,
+        
+    
+        this.getNome= function(){
+            return this.nome
+        },
+    
+        this.getIdade= function(){
+            return this.idade
+        },
+    
+        this.setNome= function(nome){
+            this.nome= nome
+        },
+    
+        this.setIdade= function(idade){
+            this.nome= idade
+        }
+    }
+    
 
 let pessoas= []
 
@@ -44,7 +71,7 @@ botao.addEventListener("click", (evt)=>{
     const nome = document.getElementById("f_nome")
     const idade = document.getElementById("f_idade")
     
-    const p= new pessoa(nome.value, idade.value)
+    const p= new Pessoa(nome.value, idade.value)
 
     pessoas.push(p)
 

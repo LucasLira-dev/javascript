5a031d5921156a19ba1aee69d686d9371051de52
lucas mendes <lucasmndes.dev@gmail.com>
2025-01-02 14:39:06 -0300

    aprendendo sobre import, export e importação nomeada

--- exercicios/CFBcursos/modulos/principal.js
+++ exercicios/CFBcursos/modulos/principal.js
@@ -1,5 +1,8 @@
-import {cursos, clubes} from "./script2.js";
-
+import getTodosCursos, {cursos, clubes} from "./script2.js";
+import *as m_curso from "./script2.js";//renomeia o arquivo script2.js para m_curso e importa tudo que está no arquivo script2.js
 
 console.log("olá mundo");
-console.log(cursos, clubes);
+console.log(m_curso.cursos);
+console.log(m_curso.clubes);
+console.log(m_curso.GetCurso(1)); //retorna o curso na posição indicada
+console.log(m_curso.default())//retorna a função getTodosCursos
--- exercicios/CFBcursos/modulos/script2.js
+++ exercicios/CFBcursos/modulos/script2.js
@@ -2,6 +2,14 @@ const cursos=["javascript", "HTML", "CSS", "PHP", "Python", "Ruby", "Java"];
 
 const clubes= ["Santos", "Corinthians", "Palmeiras", "São Paulo", "Flamengo", "Vasco", "Botafogo"];
 
+export default function getTodosCursos(){
+    return cursos;
+} //exporta a função getCursos para ser utilizada em outros arquivos. ao usar FUNCTION não é necessário usar o nome da função entre chaves e pode por o default antes da função   
 
+const GetCurso=(c)=>{
+    return cursos[c]
+} //função que retorna o curso na posição c
 
-export{cursos, clubes} //exporta a constante cursos
+export{cursos, clubes} //exporta as constantes cursos e clubes para serem utilizadas em outros arquivos
+
+export{GetCurso} //exporta a função GetCurso para ser utilizada em outros arquivos

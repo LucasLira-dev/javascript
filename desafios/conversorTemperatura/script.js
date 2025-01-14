236b2ea6f3a55373b2d003a7187f5b9450ad079f
lucas mendes <lucasmndes.dev@gmail.com>
2025-01-14 13:36:16 -0300

    terminando de criar um conversor de temperatura

--- desafios/conversorTemperatura/script.js
+++ desafios/conversorTemperatura/script.js
@@ -7,18 +7,29 @@ let rangeFahrenheit= document.getElementById("r_fahrenheit")
 let inputKelvin= document.getElementById("n_kelvin")
 let rangeKelvin= document.getElementById("r_kelvin")
 
+let btn_zerar= document.getElementById("btn_zerar")
+
 // Função para atualizar o valor do input quando o range mudar
 rangeCelsius.oninput = function() {
   inputCelsius.value = rangeCelsius.value;
-  inputFahrenheit.value= (rangeCelsius.value*9/5)+32
+  inputFahrenheit.value= ((rangeCelsius.value*9/5)+32).toFixed(2)
+  inputKelvin.value= ((parseFloat(rangeCelsius.value)) +273.15).toFixed(2)
 };
 
 rangeFahrenheit.oninput = function(){
     inputFahrenheit.value= rangeFahrenheit.value;
-    inputCelsius.value= (rangeFahrenheit.value -32)* 5/9
+    inputCelsius.value= ((rangeFahrenheit.value -32)* 5/9).toFixed(2)
+    inputKelvin.value=((rangeFahrenheit.value -32) *5/9 + 273.15).toFixed(2)
 }
 
 rangeKelvin.oninput= function(){
    inputKelvin.value= rangeKelvin.value 
-   inputFahrenheit.value= (rangeKelvin.value-273,15)*1.8+32
+   inputFahrenheit.value= ((rangeKelvin.value-273.15)*1.8+32).toFixed(2)
+   inputCelsius.value= (rangeKelvin.value- 273.15).toFixed(2)
 }
+
+btn_zerar.addEventListener("click", (evt)=>{
+  inputCelsius.value= "0"
+  inputFahrenheit.value= "0"
+  inputKelvin.value= "0"
+})

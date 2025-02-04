4823cee757253603aeb297d497bcd7e81f9fd800
lucas mendes <lucasmndes.dev@gmail.com>
2025-02-04 13:37:57 -0300

    criando a tela de login com js

--- exercicios/CFBcursos/Tela-Login/login.js
+++ exercicios/CFBcursos/Tela-Login/login.js
@@ -6,10 +6,18 @@ class Login{
     static acessologado= null;
     static estiloCss= null;
     static endpoint= "http://localhost:8000" ;
+    static config={
+        cor: "#008",
+        img: "../headerReaproveitavel/download.jpg"
+    }
     
 
-    static login=(mat, pas)=>{
-        this.endpoint+= `?matricula=${mat}&senha=${pas}`;
+    static login=(config=null)=>{
+        if(config!=null){
+            this.config= config;
+        }
+    
+
         this.estiloCss=  `*{
             margin: 0;
             padding: 0;
@@ -89,10 +97,11 @@ class Login{
 
         .botoesLogin button {
             cursor: pointer;
-            background-color: #008;
+            background-color: ${this.config.cor};
             color: white;
             border-radius: 5px;
             padding: 10px;
+            margin: 5px;
             width: 100px;
         }
     `;
@@ -103,46 +112,130 @@ class Login{
     styleEstilo.setAttribute("type", "text/css");
     styleEstilo.innerHTML= this.estiloCss;
     document.head.appendChild(styleEstilo);
-        fetch(this.endpoint)
+
+    const corpo= document.body;
+
+    const fundoLogin= document.createElement("div");
+    fundoLogin.setAttribute("id", "fundoLogin");
+    fundoLogin.setAttribute("class", "fundoLogin");
+    document.body.prepend(fundoLogin);
+    
+    const baseLogin= document.createElement("div");
+    baseLogin.setAttribute("id", "baseLogin");
+    baseLogin.setAttribute("class", "baseLogin");
+    fundoLogin.appendChild(baseLogin);
+
+    const elementosLogin= document.createElement("div");
+    elementosLogin.setAttribute("id", "elementosLogin");
+    elementosLogin.setAttribute("class", "elementosLogin");
+    baseLogin.appendChild(elementosLogin);
+
+    const campoLoginUsername= document.createElement("div");
+    campoLoginUsername.setAttribute("id", "campoLoginUsername");
+    campoLoginUsername.setAttribute("class", "campoLogin");
+    elementosLogin.appendChild(campoLoginUsername);
+
+    const labelUsername= document.createElement("label");
+    labelUsername.innerHTML= "Username";
+    campoLoginUsername.appendChild(labelUsername);
+
+    const inputUsername= document.createElement("input");
+    inputUsername.setAttribute("type", "text");
+    inputUsername.setAttribute("name", "f_username");
+    inputUsername.setAttribute("id", "f_username");
+    campoLoginUsername.appendChild(inputUsername); 
+
+    const campoLoginSenha= document.createElement("div");
+    campoLoginSenha.setAttribute("id", "campoLoginSenha");
+    campoLoginSenha.setAttribute("class", "campoLogin");
+    elementosLogin.appendChild(campoLoginSenha);
+
+    const labelSenha= document.createElement("label");
+    labelSenha.innerHTML= "Senha";
+    campoLoginSenha.appendChild(labelSenha);
+
+    const inputSenha= document.createElement("input");
+    inputSenha.setAttribute("type", "password");
+    inputSenha.setAttribute("name", "f_senha");
+    inputSenha.setAttribute("id", "f_senha");
+    campoLoginSenha.appendChild(inputSenha); 
+
+
+    // botoes
+    const botoesLogin= document.createElement("div");
+    botoesLogin.setAttribute("class", "botoesLogin");
+    elementosLogin.appendChild(botoesLogin);
+
+    const btnLogin= document.createElement("button");
+    btnLogin.setAttribute("id", "btn_login");
+    btnLogin.innerHTML= "Login";
+    btnLogin.addEventListener("click", (evt)=>{
+        this.verificaLogin()
+    }); // verifica se o login é válido
+    botoesLogin.appendChild(btnLogin);
+
+    const btnCancelar= document.createElement("button");
+    btnCancelar.setAttribute("id", "btn_cancelar");
+    btnCancelar.innerHTML= "Cancelar";
+    btnCancelar.addEventListener("click", (evt)=>{
+        this.fechar();
+    }); // fecha a tela de login quando clicar no botão cancelar
+    botoesLogin.appendChild(btnCancelar);
+
+
+
+    // logo-login
+    const logoLogin= document.createElement("div");
+    logoLogin.setAttribute("id", "logoLogin");
+    logoLogin.setAttribute("class", "logoLogin");
+    baseLogin.appendChild(logoLogin);
+
+    const imgLogo= document.createElement("img");
+    imgLogo.setAttribute("src", this.config.img);
+    imgLogo.setAttribute("alt", "");
+    logoLogin.appendChild(imgLogo);
+
+
+
+       
+    }
+
+
+    static verificaLogin=()=>{
+        const mat= document.getElementById("f_username").value;
+        const pas= document.getElementById("f_senha").value;
+ 
+        const endpoint= `http://localhost:8000?matricula=${mat}&senha=${pas}`;
+        fetch(endpoint)
         .then(res=>res.json())
         .then(res=>{
-           if(res){
-                console.log(res.acesso);
+             if(res.nome && res.acesso){ 
+               
                 this.logado= true;
                 this.matlogado= mat;
                 this.nomelogado= res.nome;
                 this.acessologado= res.acesso;
-           } else{
-               console.log("Erro no login");
-           }
+                this.fechar();
+            } else{
+                this.logado= false;
+                this.matlogado= null;
+                this.nomelogado= null;
+                this.acessologado= null;
+                alert("Credenciais inválidas");
+           } 
         })
-    }
+
+       
+    } // verifica se o login é válido
+
+    static fechar =()=>{
+        document.getElementById("fundoLogin").remove();
+        document.getElementById("id_estiloLogin").remove();
+    } // fecha a tela de login
+
 }
 
 export {Login}
 
 
 
-
-/*var http = require('http');
-var url = require('url');
-http.createServer(function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.writeHead(200, {'Content-Type': 'apllication/json'});
-
-    let parametros= url.parse(req.url, true);
-
-    let mat= parametros.query.matricula;
-    let pas= parametros.query.senha;
-
-    let dados= null;
-
-    if(mat=="123" && pas=="321"){
-        dados= {
-            nome: "Bruno",
-            acesso: 10
-        }
-    } 
-
-    res.end(JSON.stringify(dados));
-}).listen(8000); */

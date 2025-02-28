65a0e07030c4275795c00af3a0fbfaf59d6113f9
lucas mendes <lucasmndes.dev@gmail.com>
2025-02-28 12:59:26 -0300

    criando div para cada telefone adicionado

--- exercicios/CFBcursos/estoque/backoffices/colaboradores/colaboradores.js
+++ exercicios/CFBcursos/estoque/backoffices/colaboradores/colaboradores.js
@@ -1,4 +1,15 @@
 const dadosGrid= document.getElementById('dadosGrid');
+const btn_add= document.getElementById('btn_add');
+const novoColaborador= document.getElementById('novoColaborador');
+const btn_fechar= document.getElementById('btn_fechar');
+const btn_gravar= document.getElementById('btn_gravar');
+const btn_cancelar= document.getElementById('btn_cancelar');
+const f_tipoColab= document.getElementById('f_tipoColab');
+const telefones= document.getElementById('telefones');
+const f_telefone= document.getElementById('f_telefone');
+//const btn_delTel= document.getElementById('btn_delTel'); //
+
+
 
 const endpoint_todoscolaboradores= `http://127.0.0.1:1880/todosusuarios`;
 fetch(endpoint_todoscolaboradores)
@@ -39,3 +50,55 @@ fetch(endpoint_todoscolaboradores)
         dadosGrid.appendChild(linhaGrid);
     })
 });
+
+const endpoint_tiposColab= `http://127.0.0.1:1880/tiposcolab`;
+fetch(endpoint_tiposColab)
+.then(res=> res.json())
+.then(res=>{
+   f_tipoColab.innerHTML= '';
+    res.forEach(item=>{
+         const opt= document.createElement('option');
+         opt.setAttribute('value', item.n_tipousuario_tipousuario);
+         opt.textContent= item.s_desc_tipousuario;
+         f_tipoColab.appendChild(opt);
+    })
+   
+});
+
+btn_add.addEventListener('click', (evt)=>{
+    novoColaborador.classList.remove('ocultarPopup')
+})
+
+btn_fechar.addEventListener('click', (evt)=>{
+    novoColaborador.classList.add('ocultarPopup')
+})
+
+btn_cancelar.addEventListener('click', (evt)=>{
+    novoColaborador.classList.add('ocultarPopup')
+})
+
+btn_gravar.addEventListener('click', (evt)=>{
+    novoColaborador.classList.add('ocultarPopup')
+})
+
+f_telefone.addEventListener('keyup', (evt)=>{
+    if(evt.key==="Enter" && evt.target.value.trim()!==''){
+        const divTel= document.createElement('div');
+        divTel.setAttribute('class', 'tel');
+        const divNumTel= document.createElement('div');
+        divNumTel.setAttribute('class', 'numTel');
+        divNumTel.textContent= evt.target.value;
+        const Lixeira= document.createElement('img');
+        Lixeira.setAttribute('src', '../../imgs/deletar.svg');
+        Lixeira.setAttribute('class', 'delTel');
+        Lixeira.addEventListener('click', (evt)=>{
+            console.log(evt.target.parentElement);
+            evt.target.parentElement.remove();  
+        })
+        divTel.appendChild(divNumTel);
+        divTel.appendChild(Lixeira);
+        telefones.appendChild(divTel);
+        evt.target.value= '';
+    }
+})
+

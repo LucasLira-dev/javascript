252b9a63b2cfba4be4b70994b73ad8fd07652dc2
lucas mendes <lucasmndes.dev@gmail.com>
2025-03-13 13:12:42 -0300

    deixando o pesquisar funcional e tambem criando uma opção de mostrar todos

--- exercicios/CFBcursos/estoque/backoffices/colaboradores/colaboradores.js
+++ exercicios/CFBcursos/estoque/backoffices/colaboradores/colaboradores.js
@@ -18,6 +18,13 @@ const f_filtragem= document.getElementById('f_filtragem');
 const pesquisa= document.getElementById('pesquisa');
 const btn_fecharPopupPesq= document.getElementById('btn_fecharPopupPesq');
 const btn_pesq= document.getElementById('btn_pesq');
+const f_pesq= document.getElementById('f_pesq');
+const f_pesqId= document.getElementById('f_pesqId');
+const f_pesqNome= document.getElementById('f_pesqNome');
+const btn_listarTudo= document.getElementById('btn_listarTudo');
+
+const btn_pesquisar= document.getElementById('btn_pesquisar');
+
 
 f_filtragem.addEventListener('keyup', (evt)=>{
     const linhas= [...document.querySelectorAll('.linhaGrid')];
@@ -36,6 +43,8 @@ f_filtragem.addEventListener('keyup', (evt)=>{
 
 btn_pesq.addEventListener('click', (evt)=>{
     pesquisa.classList.remove('ocultarPopup');
+    f_pesq.value= '';
+    f_pesq.focus();
 });
 
 
@@ -43,6 +52,47 @@ btn_fecharPopupPesq.addEventListener('click', (evt)=>
 {
     pesquisa.classList.add('ocultarPopup');
 });
+
+f_pesqId.addEventListener('click', (evt)=>{
+    f_pesq.value= '';
+    f_pesq.focus();
+})
+f_pesqNome.addEventListener('click', (evt)=>{
+    f_pesq.value= '';
+    f_pesq.focus();
+})
+
+
+btn_pesquisar.addEventListener('click', (evt)=>{
+    let tipo= null;
+    if(f_pesqId.checked){
+        tipo= 'id';
+    }else{
+        tipo= 'nome';
+    }
+    if(f_pesq.value!= ''){
+        const endpointpesq= `${serv}pesquisacolab/${tipo}/${f_pesq.value}`;
+        fetch(endpointpesq)
+        .then(res=> res.json())
+        .then(res=>{
+            dadosGrid.innerHTML= '';
+            res.forEach(item=>{
+                criarLinha(item);
+            })
+        })
+
+        pesquisa.classList.add('ocultarPopup');
+    }else{
+        alert('Informe um nome ou id para pesquisar');
+        f_pesq.focus();
+    }
+});
+
+btn_listarTudo.addEventListener('click', (evt)=>{
+    carregarTodosColabs()
+});
+
+
 
 // n= novo colaborador, e= editar colaborador
 let modojanela= 'n';
@@ -103,122 +153,126 @@ fetch(endpoint_todoscolaboradores)
     dadosGrid.innerHTML= '';
 
     res.forEach(item=>{
-        const linhaGrid= document.createElement('div');
-        linhaGrid.setAttribute('class', 'linhaGrid');
-
-        const divc1= document.createElement('div');
-        divc1.setAttribute('class', 'colunaLinhaGrid c1');
-        divc1.textContent= item.n_usuario_usuario;
-        linhaGrid.appendChild(divc1);
-
-        const divc2= document.createElement('div');
-        divc2.setAttribute('class', 'colunaLinhaGrid c2');
-        divc2.textContent= item.s_nome_usuario;
-        linhaGrid.appendChild(divc2);
-
-        const divc3= document.createElement('div');
-        divc3.setAttribute('class', 'colunaLinhaGrid c3');
-        divc3.textContent= item.n_tipousuario_tipousuario
-        ;
-        linhaGrid.appendChild(divc3);
-
-        const divc4= document.createElement('div');
-        divc4.setAttribute('class', 'colunaLinhaGrid c4');
-        divc4.textContent= item.c_status_usuario;        
-        linhaGrid.appendChild(divc4);
-
-        const divc5= document.createElement('div');
-        divc5.setAttribute('class', 'colunaLinhaGrid c5');
-        linhaGrid.appendChild(divc5);
-
-        const img_status= document.createElement('img');
-        if(item.c_status_usuario==='A'){
-            img_status.setAttribute('src', '../../imgs/on.svg');
-            img_status.setAttribute('class', 'img_on iconeop');
-        }else{
-            img_status.setAttribute('src', '../../imgs/off.svg');
-            img_status.setAttribute('class', 'img_off iconeop');
-        }
-        
-        img_status.setAttribute('data-idcolab', item.n_usuario_usuario);
-        img_status.addEventListener('click', (evt)=>{
-            const idcolab= evt.target.getAttribute('data-idcolab');
-            if(evt.target.getAttribute('src').includes('on')){
-                const endpoint_mudarStatus= `${serv}mudarstatuscolab/${idcolab}/I`;
-                fetch(endpoint_mudarStatus)
-                .then(res=>{
-                    if(res.status===200){
-                        evt.target.setAttribute('src', '../../imgs/off.svg');
-                        evt.target.parentNode.parentNode.childNodes[3].textContent= 'I';
-                        evt.target.setAttribute('class', 'img_off iconeop');
-                    }
-                })
-                }else{
-                    const endpoint_mudarStatus= `${serv}mudarstatuscolab/${idcolab}/A`;
-                    fetch(endpoint_mudarStatus)
-                    .then(res=>{
-                        if(res.status==200){
-                            evt.target.setAttribute('src', '../../imgs/on.svg');
-                            evt.target.parentNode.parentNode.childNodes[3].textContent= 'A';
-                            evt.target.setAttribute('class', 'img_on iconeop');
-                        }
-                    })
-                }
-        });
-        divc5.appendChild(img_status);
-
-        const img_editar= document.createElement('img');
-        img_editar.setAttribute('src', '../../imgs/editar.svg');
-        img_editar.setAttribute('class', 'img_editar iconeop');
-        img_editar.addEventListener('click', (evt)=>{
-            modojanela= 'e';
-            telefones.innerHTML= '';
-            document.getElementById('tituloPopup').textContent= 'Editar Colaborador';
-
-            const id= evt.target.parentNode.parentNode.firstChild.innerHTML;
-
-            let endpoint= `${serv}dadoscolab/${id}`;
-            fetch(endpoint)
-            .then(res=> res.json())
-            .then(res=>{
-                btn_gravar.setAttribute('data-idcolab', id);
-
-                f_nome.value= res[0].s_nome_usuario;
-                f_tipoColab.value= res[0].n_tipousuario_tipousuario;
-                f_status.value= res[0].c_status_usuario;
-                img_foto.src= res[0].s_foto_usuario;
-
-
-                novoColaborador.classList.remove('ocultarPopup');
-            })
-
-            endpoint= `${serv}telefonescolab/${id}`;
-            fetch(endpoint)
-            .then(res=> res.json())
-            .then(res=>{
-                res.forEach(t=>{
-                    criarCxTelefone(t.s_numero_telefone, t.n_telefone_telefone, "e");
-                    carregarTodosColabs
-                })
-                
-                novoColaborador.classList.remove('ocultarPopup');
-            })
-
-
-        })
-        divc5.appendChild(img_editar);
-
-        const img_excluir= document.createElement('img');
-        img_excluir.setAttribute('src', '../../imgs/deletar.svg');
-        img_excluir.setAttribute('class', 'img_excluir iconeop');
-        divc5.appendChild(img_excluir);
-
-        dadosGrid.appendChild(linhaGrid);
+      criarLinha(item);
     })
 });
 }
 
 carregarTodosColabs();
+
+const criarLinha=(item)=>{
+    const linhaGrid= document.createElement('div');
+    linhaGrid.setAttribute('class', 'linhaGrid');
+
+    const divc1= document.createElement('div');
+    divc1.setAttribute('class', 'colunaLinhaGrid c1');
+    divc1.textContent= item.n_usuario_usuario;
+    linhaGrid.appendChild(divc1);
+
+    const divc2= document.createElement('div');
+    divc2.setAttribute('class', 'colunaLinhaGrid c2');
+    divc2.textContent= item.s_nome_usuario;
+    linhaGrid.appendChild(divc2);
+
+    const divc3= document.createElement('div');
+    divc3.setAttribute('class', 'colunaLinhaGrid c3');
+    divc3.textContent= item.n_tipousuario_tipousuario
+    ;
+    linhaGrid.appendChild(divc3);
+
+    const divc4= document.createElement('div');
+    divc4.setAttribute('class', 'colunaLinhaGrid c4');
+    divc4.textContent= item.c_status_usuario;        
+    linhaGrid.appendChild(divc4);
+
+    const divc5= document.createElement('div');
+    divc5.setAttribute('class', 'colunaLinhaGrid c5');
+    linhaGrid.appendChild(divc5);
+
+    const img_status= document.createElement('img');
+    if(item.c_status_usuario==='A'){
+        img_status.setAttribute('src', '../../imgs/on.svg');
+        img_status.setAttribute('class', 'img_on iconeop');
+    }else{
+        img_status.setAttribute('src', '../../imgs/off.svg');
+        img_status.setAttribute('class', 'img_off iconeop');
+    }
+    
+    img_status.setAttribute('data-idcolab', item.n_usuario_usuario);
+    img_status.addEventListener('click', (evt)=>{
+        const idcolab= evt.target.getAttribute('data-idcolab');
+        if(evt.target.getAttribute('src').includes('on')){
+            const endpoint_mudarStatus= `${serv}mudarstatuscolab/${idcolab}/I`;
+            fetch(endpoint_mudarStatus)
+            .then(res=>{
+                if(res.status===200){
+                    evt.target.setAttribute('src', '../../imgs/off.svg');
+                    evt.target.parentNode.parentNode.childNodes[3].textContent= 'I';
+                    evt.target.setAttribute('class', 'img_off iconeop');
+                }
+            })
+            }else{
+                const endpoint_mudarStatus= `${serv}mudarstatuscolab/${idcolab}/A`;
+                fetch(endpoint_mudarStatus)
+                .then(res=>{
+                    if(res.status==200){
+                        evt.target.setAttribute('src', '../../imgs/on.svg');
+                        evt.target.parentNode.parentNode.childNodes[3].textContent= 'A';
+                        evt.target.setAttribute('class', 'img_on iconeop');
+                    }
+                })
+            }
+    });
+    divc5.appendChild(img_status);
+
+    const img_editar= document.createElement('img');
+    img_editar.setAttribute('src', '../../imgs/editar.svg');
+    img_editar.setAttribute('class', 'img_editar iconeop');
+    img_editar.addEventListener('click', (evt)=>{
+        modojanela= 'e';
+        telefones.innerHTML= '';
+        document.getElementById('tituloPopup').textContent= 'Editar Colaborador';
+
+        const id= evt.target.parentNode.parentNode.firstChild.innerHTML;
+
+        let endpoint= `${serv}dadoscolab/${id}`;
+        fetch(endpoint)
+        .then(res=> res.json())
+        .then(res=>{
+            btn_gravar.setAttribute('data-idcolab', id);
+
+            f_nome.value= res[0].s_nome_usuario;
+            f_tipoColab.value= res[0].n_tipousuario_tipousuario;
+            f_status.value= res[0].c_status_usuario;
+            img_foto.src= res[0].s_foto_usuario;
+
+
+            novoColaborador.classList.remove('ocultarPopup');
+        })
+
+        endpoint= `${serv}telefonescolab/${id}`;
+        fetch(endpoint)
+        .then(res=> res.json())
+        .then(res=>{
+            res.forEach(t=>{
+                criarCxTelefone(t.s_numero_telefone, t.n_telefone_telefone, "e");
+                carregarTodosColabs
+            })
+            
+            novoColaborador.classList.remove('ocultarPopup');
+        })
+
+
+    })
+    divc5.appendChild(img_editar);
+
+    const img_excluir= document.createElement('img');
+    img_excluir.setAttribute('src', '../../imgs/deletar.svg');
+    img_excluir.setAttribute('class', 'img_excluir iconeop');
+    divc5.appendChild(img_excluir);
+
+    dadosGrid.appendChild(linhaGrid);
+}
 
 const endpoint_tiposColab= `${serv}tiposcolab`;
 fetch(endpoint_tiposColab)
